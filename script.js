c2ef9c96ed4a44505ff0fed25eef16372e7c6f0f
W0823713 <134808337+W0823713@users.noreply.github.com>
2023-10-23 23:08:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+/*
+    Assignment 05
+*/
+
+$(document).ready(function () {
+    class ContentItem {
+        constructor(id, name, description, category) {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.category = category;
+        }
+
+        updateContentItem(id, name, description, category) {
+            if (id === this.id) {
+                if (name !== null) {
+                    this.name = name;
+                }
+                if (description !== null) {
+                    this.description = description;
+                }
+                if (category !== null) {
+                    this.category = category;
+                }
+            }
+        }
+
+        toString() {
+            const contentItemHtml = `
+                <div class="content-item-wrapper" id="content-item-${this.id}">
+                    <h2>${this.name}</h2>
+                    <p>${this.description}</p>
+                    <div>${this.category}</div>
+                </div>
+            `;
+            return contentItemHtml;
+        }
+    }
+
+    const contentItems = [
+        new ContentItem(0, "Sedan", "A sleek and stylish sedan for urban driving.", "Compact Cars"),
+        new ContentItem(1, "SUV", "A rugged and versatile SUV for outdoor adventures.", "Sport Utility Vehicles"),
+        new ContentItem(2, "Sports Car", "A high-performance sports car built for speed.", "Sports Cars"),
+        new ContentItem(3, "Truck", "A powerful and capable truck for heavy-duty tasks.", "Pickup Trucks"),
+        new ContentItem(4, "Convertible", "A convertible car that lets you enjoy the open road.", "Convertible Cars"),
+    ];
+    
+    const contentItemList = $('#content-item-list');
+    
+    contentItems.forEach(item => {
+        contentItemList.append(item.toString());
+
+        const itemWrapper = $(`#content-item-${item.id}`);
+        itemWrapper.css({
+            border: '1px solid #000',
+            width: '300px',
+            padding: '10px',
+            margin: '10px auto',
+        });
+    });
+
+    const themeName = "Your Chosen Theme";
+    $('#theme-name').text(themeName);
+
+    $('#update-success').click(function() {
+        contentItems[0].updateContentItem(0, "New Name", "New Description", "New Category");
+    });
+
+    $('#update-fail').click(function() {
+        contentItems[1].updateContentItem(0, "New Name", null, "New Category");
+    });
+});
+
+
